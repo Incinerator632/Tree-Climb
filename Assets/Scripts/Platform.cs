8313a2e3c035f9d7efe4bbe7caeb80827e22a28f
JBoremi <jboremi@student.fitchburgstate.edu>
2022-04-03 20:55:20 -0400

    Final Project
    Made Main menu screen and death screen. Made the platforms randomly generate throughout the level.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public float jumpForce = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.relativeVelocity.y <= 0f)
+        {
+            Rigidbody2D player2Rb2d = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (player2Rb2d != null)
+            {
+                Vector2 velocity = player2Rb2d.velocity;
+                velocity.y = jumpForce;
+                player2Rb2d.velocity = velocity;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D player2Rb2d;
+    private float speed = 10f;
+    private float horizontalInput;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player2Rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        player2Rb2d.velocity = new Vector2(horizontalInput * speed, player2Rb2d.velocity.y);
+    }
+}
